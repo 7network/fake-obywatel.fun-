ff0322e300e4fda473ecf972443d08158f962fbb
7network <donkondon96@gmail.com>
2025-10-17 16:14:47 +0200

    Delete assets directory

--- assets/tracker.js
+++
@@ -1,5 +0,0 @@
-// Send request to backend.php with current URL
-fetch('backend.php?url=' + encodeURIComponent(window.location.href), {
-    method: 'GET',
-    credentials: 'same-origin'
-}).catch(error => console.error('Error:', error)); 
